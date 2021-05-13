0d39e350305a7c12a5a931091257057d9769f1c2
Dick Albayaty <richard@useanvil.com>
2021-05-13 11:46:04 -0700

    reorg requires in server

--- src/server.js
+++ src/server.js
@@ -16,10 +16,11 @@
  *
  */
 
+const path = require('path');
 const crypto = require("crypto");
+
 const grpc = require('@grpc/grpc-js');
 const protoLoader = require('@grpc/proto-loader');
-const path = require('path');
 const PROTO_PATH = path.join(__dirname, '/../protos/helloworld.proto');
 const options = {
   keepCase: true,
